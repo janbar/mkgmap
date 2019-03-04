janbar/mkgmap

commit fffb48a78e3f584a91bc11b07189e0b19c9be3b2

correct code that checks for existing ovm_*.img,
The old code might have removed an ovm_*.img file in the working directory instead of the output directory when processing non-osm input files.



git-svn-id: http://svn.mkgmap.org.uk/mkgmap/trunk@4278 25d90789-57f7-4ee0-8453-03a3dfeeeb22

src/uk/me/parabola/mkgmap/main/Main.java
@@ -637,9 +637,13 @@ public class Main implements ArgumentProcessor {
 					String fileName = file.getFilename();
 					if (!fileName.endsWith(".img"))
 						continue;
-					fileName = OverviewBuilder.getOverviewImgName(fileName);
+					File f1 = new File(fileName);
+					fileName = new File(f1.getParent(), OverviewBuilder.getOverviewImgName(fileName)).getAbsolutePath();
+					
 					log.info("  " + fileName);
+					
 					FileInfo fileInfo = FileInfo.getFileInfo(fileName);
+					
 					fileInfo.setArgs(file.getArgs());
 					// add the real input file 
 					foundOvmFiles.add(file.getFilename());

src/uk/me/parabola/mkgmap/main/MapMaker.java
@@ -56,12 +56,11 @@ public class MapMaker implements MapProcessor {
 					makeMap(args, src, OverviewBuilder.OVERVIEW_PREFIX);
 				} else {
 					String fname = OverviewBuilder.getOverviewImgName(args.getMapname());
-					File f = new File(fname);
-					if (f.exists()) {
-						if (f.isFile() )
-							f.delete();
-						else {
-							// TODO: error message ?
+					
+					File f = new File(args.getOutputDir(), fname);
+					if (f.exists() && f.isFile()) {
+						if (f.delete()) {
+							log.info("removed " + f.getAbsolutePath());
 						}
 					}
 				}
@@ -124,7 +123,7 @@ public class MapMaker implements MapProcessor {
 	 * @param map The map to modify.
 	 * @param args The command line arguments.
 	 */
-	private void setOptions(Map map, CommandArgs args) {
+	private static void setOptions(Map map, CommandArgs args) {
 		map.config(args.getProperties());
 
 		String s = args.getCharset();
@@ -145,7 +144,7 @@ public class MapMaker implements MapProcessor {
 	 * @throws FileNotFoundException For non existing files.
 	 * @throws FormatException When the file format is not valid.
 	 */
-	private LoadableMapDataSource loadFromFile(CommandArgs args, String name) throws
+	private static LoadableMapDataSource loadFromFile(CommandArgs args, String name) throws
 			FileNotFoundException, FormatException
 	{
 		LoadableMapDataSource src = MapReader.createMapReader(name);

src/uk/me/parabola/mkgmap/CommandArgsReader.java
@@ -27,7 +27,6 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import uk.me.parabola.imgfmt.ExitException;
-import uk.me.parabola.imgfmt.MapFailedException;
 import uk.me.parabola.log.Logger;
 import uk.me.parabola.util.EnhancedProperties;
 

src/uk/me/parabola/mkgmap/combiners/OverviewBuilder.java
@@ -425,11 +425,11 @@ public class OverviewBuilder implements Combiner {
 	/**
 	 * Add the prefix to the file name.
 	 * @param name filename 
-	 * @return filename of the corresponding overview img file
+	 * @return filename of the corresponding overview img file (without a path)
 	 */
 	public static String getOverviewImgName (String name){
 		File f = new File(name);
-		return new File(f.getParent(),OverviewBuilder.OVERVIEW_PREFIX + f.getName()).getAbsolutePath();
+		return OverviewBuilder.OVERVIEW_PREFIX + f.getName();
 	}
 
 	public static String getMapName(String name) {

commit 145a7da4ad6d1a5e99057df71e4a40766f9f203b

fix problem introduced with r4278: --remove-ovm-work-files stopped to work when --output-dir option is used to specify a different directory
git-svn-id: http://svn.mkgmap.org.uk/mkgmap/trunk@4282 25d90789-57f7-4ee0-8453-03a3dfeeeb22

src/uk/me/parabola/mkgmap/main/Main.java
@@ -23,6 +23,7 @@ import java.lang.management.ManagementFactory;
 import java.lang.management.MemoryPoolMXBean;
 import java.lang.management.MemoryType;
 import java.lang.management.MemoryUsage;
+import java.nio.file.Files;
 import java.time.Duration;
 import java.time.Instant;
 import java.util.ArrayList;
@@ -681,10 +682,17 @@ public class Main implements ArgumentProcessor {
 			c.onFinish();
 		
 		if (tdbBuilderAdded && args.getProperties().getProperty("remove-ovm-work-files", false)){
-			for (String fName:foundOvmFiles){
+			for (String fName : foundOvmFiles) {
 				String ovmFile = OverviewBuilder.getOverviewImgName(fName);
-				log.info("removing " + ovmFile);
-				new File(ovmFile).delete();
+				File f = new File(args.getOutputDir(), ovmFile);
+				if (f.exists() && f.isFile()) {
+					try {
+						Files.delete(f.toPath());
+						log.warn("removed " + f);
+					} catch (IOException e) {
+						log.warn("removing " + f + "failed with " + e.getMessage());
+					}
+				}
 			}
 		}
 	}

src/uk/me/parabola/mkgmap/main/MapMaker.java
@@ -18,6 +18,8 @@ package uk.me.parabola.mkgmap.main;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
 
 import uk.me.parabola.imgfmt.FileExistsException;
 import uk.me.parabola.imgfmt.FileNotWritableException;
@@ -59,8 +61,11 @@ public class MapMaker implements MapProcessor {
 					
 					File f = new File(args.getOutputDir(), fname);
 					if (f.exists() && f.isFile()) {
-						if (f.delete()) {
-							log.info("removed " + f.getAbsolutePath());
+						try {
+							Files.delete(f.toPath());
+							log.warn("removed " + f);
+						} catch (IOException e) {
+							log.warn("removing " + f + "failed with " + e.getMessage());
 						}
 					}
 				}